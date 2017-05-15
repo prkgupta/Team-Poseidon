a80ffb82694d29dd7d4c05c1f1c1021421b2cb4f
aollok <aitorolloqui@hotmail.com>
2017-05-14 18:58:44 -0700

    updated pipeline

--- test/test.js
+++ test/test.js
@@ -1,8 +1,17 @@
 import {describe, it} from 'mocha';
 import {expect} from 'chai';
+import config from '../server/config/config';
 
-describe ("test", function(){
+describe("config test", function () {
 	it("should work", function () {
 		expect(true).to.equal(true);
 	});
+
+	it('should have correct config', function () {
+	    expect(config).to.deep.equal({
+            mongoDBURI: process.env.MONGOLAB_URI || 'mongodb://admin:admin@ds061335.mongolab.com:61335/heroku_w9bxpzpc',
+            port: process.env.PORT || 4941,
+            secret: process.env.SECRET || 'mysecret'
+        });
+    });
 });
