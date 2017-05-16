c129396dd330193c7c7667663914cf8bc45b5dbe
HaeJoon Yoon <haejoonyoon@HaeJoons-MacBook-Pro.local>
2017-05-16 00:41:43 -0700

    one minor fix

--- test/e2e/e2e.js
+++ test/e2e/e2e.js
@@ -3,21 +3,21 @@ var config = require('../../nightwatch.conf.BASIC.js');
 module.exports = { // adapted from: https://git.io/vodU0
   'Poseidon': function(browser) {
     browser
-      .url('http://localhost:4400')
+      .url('http://localhost:4000')
       .waitForElementVisible('body', 5000)
       .saveScreenshot('localhost')
       .end();
   }
 };
 
-module.exports = {
-  'Login test': function (client) {
-    client
-      .url('http://localhost:5300')
-      .setValue('input[name="email"]', 'abc@gmail.com')
-      .setValue('input[name="password]', 'abcabc')
-      .click('button[type="submit"]')
-      .assert.containsText('main', 'News feed')
-      .end();
-  }
-};
+// module.exports = {
+//   'Login test': function (client) {
+//     client
+//       .url('http://localhost:5300')
+//       .setValue('input[name="email"]', 'abc@gmail.com')
+//       .setValue('input[name="password]', 'abcabc')
+//       .click('button[type="submit"]')
+//       .assert.containsText('main', 'News feed')
+//       .end();
+//   }
+// };
