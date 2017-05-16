6825ee521ad9640dc3127aaa3f4a323cc90cddad
HaeJoon Yoon <haejoonyoon@HaeJoons-MacBook-Pro.local>
2017-05-16 00:30:51 -0700

    nightwatch

---
+++ test/e2e/e2e.js
@@ -0,0 +1,23 @@
+var config = require('../../nightwatch.conf.BASIC.js');
+
+module.exports = { // adapted from: https://git.io/vodU0
+  'Poseidon': function(browser) {
+    browser
+      .url('http://localhost:4400')
+      .waitForElementVisible('body', 5000)
+      .saveScreenshot('localhost')
+      .end();
+  }
+};
+
+module.exports = {
+  'Login test': function (client) {
+    client
+      .url('http://localhost:5300')
+      .setValue('input[name="email"]', 'abc@gmail.com')
+      .setValue('input[name="password]', 'abcabc')
+      .click('button[type="submit"]')
+      .assert.containsText('main', 'News feed')
+      .end();
+  }
+};
