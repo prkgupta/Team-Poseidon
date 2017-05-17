a1c8581071dfc3346f53973ecfc62ea884656814
rmanzano <rmanzano@ucsd.edu>
2017-05-17 12:25:22 -0700

    update .env file name in conf file

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,4 +1,4 @@
-require('env2')('.env'); // optionally store your environment variables in .env
+require('env2')('simple.env'); // optionally store your environment variables in .env
 const PKG = require('./package.json'); // so we can get the version of the project
 const BINPATH = './node_modules/nightwatch/bin/'; // change if required.
 const SCREENSHOT_PATH = "./screenshots/" + PKG.version + "/";
